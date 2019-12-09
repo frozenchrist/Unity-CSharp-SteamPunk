ab3bc8e9a60a7f1f308b502735393dae10b01523
Chris Xie <frozenchrist@gmail.com>
2019-12-09 17:16:20 -0500

    version 1.0

---
+++ SteamPunk/Assets/Scripts/SteamPunk.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+
+public class SteamPunk : MonoBehaviour
+{
+
+    [SerializeField] TextMeshProUGUI textComponent;
+    [SerializeField] State startingState;
+
+
+    State state;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        state = startingState;
+
+        
+
+        textComponent.text = state.GetStateStory();
+
+        textComponent.fontSize = 50;
+
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        ManageState();
+
+        
+    }
+
+    private void ManageState(){
+
+        
+        var nextState = state.getNextStates();
+
+        for (int i = 0; i < nextState.Length ; i++) {
+
+            if (Input.GetKeyDown(KeyCode.Alpha1 + i))
+            {
+
+                state = nextState[i];
+
+            }
+            
+
+        }
+
+
+        
+        
+        textComponent.text = state.GetStateStory();
+
+
+    }
+}
